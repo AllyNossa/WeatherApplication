e21996a45011aa4a8943d3d936b9d70dc3d3f68d
Alla Nosova <aknosova@avito.ru>
2019-07-14 00:59:07 +0300

    Домашка 5 мелкая доработка

--- app/src/main/java/com/aknosova/weatherapplication/MainActivity.java
+++ app/src/main/java/com/aknosova/weatherapplication/MainActivity.java
@@ -1,24 +1,12 @@
 package com.aknosova.weatherapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity {
-//    private Button searchButton;
-//    private EditText editTextCity;
-
-//    private String inputTextCity;
-//    private CheckBox humidityParam;
 
     SearchCityFragment searchCityFragment;
     FragmentTransaction fragmentTransaction;
@@ -30,47 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
         searchCityFragment = new SearchCityFragment();
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.main_container, searchCityFragment).addToBackStack(null).commit();
-
-
+        fragmentTransaction.add(R.id.main_container, searchCityFragment).commit();
     }
-
-//        searchButton = findViewById(R.id.search_button);
-//        editTextCity = findViewById(R.id.search_input);
-//        humidityParam = findViewById(R.id.cb_humidity);
-//
-//        searchButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (editTextCity.getText().length() == 0) {
-//                    Toast.makeText(MainActivity.this, getString(R.string.error_empty_value), Toast.LENGTH_SHORT).show();
-//                } else {
-//                    LocalParcel parcel = new LocalParcel();
-//                    parcel.setText(editTextCity.getText().toString());
-//                    parcel.setChecked(isCheckedCheckbox());
-//                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//                    intent.putExtra(STATE, parcel);
-//                    startActivity(intent);
-//                }
-//            }
-//        });
-//    }
-//
-//    private boolean isCheckedCheckbox() {
-//        return humidityParam.isChecked();
-//    }
-//
-//    @Override
-//    public void onSaveInstanceState(@NonNull Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//        inputTextCity = editTextCity.getText().toString();
-//        outState.putString(STATE, inputTextCity);
-//    }
-//
-//    @Override
-//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-//        super.onRestoreInstanceState(savedInstanceState);
-//        inputTextCity = savedInstanceState.getString(STATE);
-//        editTextCity.setText(inputTextCity);
-//    }
 }
